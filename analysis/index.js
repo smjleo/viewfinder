49c3fba8e088ffcd526e6fe33ba084508c7f043a
virchau13 <computernerd7777@gmail.com>
2020-05-24 04:36:37 +0800

    basic opinion analysis

--- analysis/index.js
+++ analysis/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
 const lang = require('@google-cloud/language');
+let isControversial = require('./controversial.js');
 
 let app = express();
 const client = new lang.LanguageServiceClient();
@@ -14,21 +15,69 @@ app.post('/getOpinion', (req, res) => {
         res.end();
         return;
     }
-        client.analyzeEntities({
+    client.analyzeEntitySentiment({
         document: {
             content: req.body.text,
+            language: "EN",
             type: 'PLAIN_TEXT'
-        }
+        },
+        "encodingType": "UTF8"
     })
         /* only the FIRST document */
-        .then(data => res.send(data).end())
-        /* .then(data => data[0])
-        .then(data => 
-            data.entities
-                .filter(entity => entity.mentions.sentiment.magnitude >= 0.5)
-                .map(entity => ({
-                })
-        ); */
+        .then(data => data[0])
+        .then(async data => {
+            // Result array
+            let result = [];
+            // topic cache in case we run into duplicates
+            let topicCache = {};
+            for(let i = 0; i < data.entities.length; i++){
+                console.log("lmao");
+                // The threshold at which to consider the opinion volatile.
+                const SENTIMENT_THRESHOLD = 0.5;
+                let entity = data.entities[i];
+                // Check cache
+                let url;
+                if(topicCache[entity.name]){
+                    // just don't do anything
+                    continue;
+                } else {
+                    url = await isControversial(entity.name);
+                    topicCache[entity.name] = url;
+                }
+                // Check if the topic is controversial,
+                // if it is this URL will have a url in it
+                console.log(url);
+                if(url){
+                    // Check all volatile mentions of the topic, and get the opinion.
+                    // If it swings between bad and good, then the answer is "mixed".
+                    // If it stays good or stays bad, the answer is "positive" or "negative".
+                    // If it doesn't run into volatile opinions, it will be "neutral".
+                    let sign = 0;
+                    let opinion = "neutral";
+                    for(let j = 0; j < entity.mentions.length; j++){
+                        let score = entity.mentions[j].sentiment.score;
+                        if(Math.abs(score) >= SENTIMENT_THRESHOLD){
+                            if(sign && Math.sign(score) !== sign){
+                                opinion = "mixed";
+                                sign = 0;
+                                break;
+                            } else {
+                                sign = Math.sign(score);
+                            }
+                        }
+                    }
+                    if(sign){
+                        opinion = (sign > 0 ? "positive" : "negative");
+                    }
+                    result.push({
+                        topic: entity.name,
+                        opinion,
+                        link: url
+                    });
+                }
+            }
+            res.send(result);
+        });
 });
 
 app.listen(5678);

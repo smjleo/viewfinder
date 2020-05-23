370082f30babbbdc6447b749ad7e03fb811d7899
sm49697 <stamaria49697@sas.edu.sg>
2020-05-24 02:48:05 +0800

    added base for POST join/leave rooms

--- chat/app.js
+++ chat/app.js
@@ -3,13 +3,42 @@ const app = express();
 
 const CHAT_PORT = 4000;
 
+// Hacked a json database, since it's a hackathon
+const DATABASE = 
+    {
+        "rooms": {
+            "example": {
+                "users": {
+
+                },
+                "messages": []
+            }
+        }
+    }
+
+// Allows the app to parse json files
 app.use(express.json);
 
-/* Handle the GET request for joining a chat room
+/* Handle the GET request for seeing a chat room
  * @param name - the name of the room (named after the controversy)
  */
 app.get('/api/room/:name', (req, res) => {
-    res.send(req.params);
+    console.log(`GET at /api/room/${req.params.name}`);
+    res.send(JSON.stringify(req.params));
+});
+
+/* Handle the POST request for joining a chat room
+ * @param name - the name of the room (named after the controversy)
+ */
+app.post('/api/room/:name/join', (req, res) => {
+
+});
+
+/* Handle the POST request for leaving a chat room
+ * @param name - the name of the room (named after the controversy)
+ */
+app.post('/api/room/:name/leave', (req, res) => {
+
 });
 
 app.listen(CHAT_PORT, () => {
--- chat/documentation.md
+++ chat/documentation.md
@@ -2,6 +2,15 @@
 
 ## Accessing Room
 
-*requires*: name the name of the room (named after the controversy)
-*effects*: returns a json containing the room information
+**requires**: **name** the name of the room (named after the controversy)
+**effects**: returns a json containing the room information
 ${url}/api/room/:name
+
+## Joining Room
+
+**requires**: **name** the name of the room (named after the controversy)
+**effects**: adds a user to the room's database
+
+## Leaving Room
+**requires**: **name** the name of the room (named after the controversy)
+**effects**: removes a user from the room's database

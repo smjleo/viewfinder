c75ac1580d6e5ad712580d7d2be4d9c6bb1196b0
claby2 <wibow9770@gmail.com>
2020-05-24 06:41:54 +0800

    made recurse function

--- extension/main.js
+++ extension/main.js
@@ -1,16 +1,16 @@
-// let controversialWords = ["problems", "Coronavirus"];
-
 function getAnalysis(_text) {
     return fetch('http://localhost:5678/getOpinion', {method : "POST", body : JSON.stringify({"text" : _text}), headers : {'Content-Type': 'application/json'}})
     .then(res => res.json())
 }
 
 
+let sleep = ms => new Promise((resolve, reject) => setTimeout(resolve, ms));
+
 setTimeout(function(){
     let text;
     let posts = [...document.querySelectorAll('div.entry')];
 
-    for(let i = 0; i < posts.length; i++) {
+    (function recurse(i) {
         let arr = [...posts[i].querySelectorAll('div.usertext-body')]; // Set arr as array of div children with usertext-body class
         if(arr.length === 0) { // Check if post is a title
             posts[i] = [...posts[i].querySelectorAll('.title')][1]; // Set posts element as title of a post
@@ -18,21 +18,14 @@ setTimeout(function(){
             posts[i] = arr[0]; // Set posts element as comment or post content
         }
 
-        setTimeout(function() {
-            if(posts[i] === undefined) {
-                continue;
-            }
-        
+        if(!(posts[i] === undefined)) {
             getAnalysis(posts[i].innerText).then(elem => {
                 console.log(elem);
             });
-        }, 1000);
-    
-        // text += posts[i].innerText;
-        // controversialWords.forEach(word => {
-        //     if(text.includes(word)) {
-        //         console.log("FOUND!");
-        //     }
-        // });
-    }
+        }
+
+        if(i + 1 < posts.length) {
+            setTimeout(recurse, 100, i + 1); // Untested might change
+        }
+    })(0);
 }, 3000); // Timeout acts as a buffer while page loads  

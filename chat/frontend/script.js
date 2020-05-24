32881a654742e8526aafa3fd68fe0c1a89effee9
sm49697 <stamaria49697@sas.edu.sg>
2020-05-24 14:01:00 +0800

    added message sending, random anonymous user handles

--- chat/frontend/script.js
+++ chat/frontend/script.js
@@ -1,25 +1,30 @@
-const usernameButton = document.getElementById('username-change');
-const usernameTextarea = document.getElementById('username');
-const errorArea = document.getElementById('error-mess');
+const messageSendButton = document.getElementById('send-button');
+const messageTextarea = document.getElementById('message-area');
 
-let usernameDisplays = [...document.getElementsByClassName('username')];
+let usernameDisplays = [...document.getElementsByClassName('user-username')];
 
 const potentialUsernames = ['Armadillo', 'Platypus', 'Cat', 'Dog', 'Elephant', 'Ferret', 'Lion', 'Cheetah', 'Senpai'];
 
 let username = 'Anonymous' + potentialUsernames[Math.floor(Math.random() * potentialUsernames.length)];
+const room = document.getElementById('room-header').innerHTML;
 
-usernameButton.addEventListener('click', () => {
-    const change = usernameTextarea.value;
-    const MAX_CHARACTERS = 30;
-    if (change.length > MAX_CHARACTERS) {
-        errorArea.innerHTML = `Too many characters! Max ${MAX_CHARACTERS}`;
+messageSendButton.addEventListener('load', () => {
+    usernameDisplays.forEach((elem) => {
+        elem.innerHTML = username;
+    });
+});
+
+messageSendButton.addEventListener('click', () => {
+    const messageInfo = {
+        "author": username,
+        "message": messageTextarea.value,
+        "time": Date.now()
     }
-    else {
-        errorArea.innerHTML = '';
-        username = change;
-        
-        usernameDisplays.forEach((elem) => {
-            elem.innerHTML = username;
-        });
-    }
+    const url = `http://localhost:4000/api/room/${room}/message`;
+    fetch(url, {
+        method: 'post',
+        body: JSON.stringify(messageInfo)
+    }).then((response) => {
+        return response.status;
+    });
 });

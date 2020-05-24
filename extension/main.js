6153d6bbc8bef156391ce785e6bd4762f8b7c8f8
virchau13 <computernerd7777@gmail.com>
2020-05-24 12:03:22 +0800

    made extension highlight words

--- extension/main.js
+++ extension/main.js
@@ -22,6 +22,24 @@ setTimeout(function(){
             getAnalysis(posts[i].innerText).then(elem => {
                 if(elem.length !== 0) {
                     console.log(elem[0]);
+                    let nodes = document.createTreeWalker(posts[i], NodeFilter.SHOW_ALL, null, null);
+                    let node, pnode;
+                    let currLength = 0;
+                    while(node = nodes.nextNode()){
+                        console.log(node);
+                        if(node.nodeName === '#text'){
+                            // We know the previous node must have had text in it.
+                            currLength += pnode.innerText.length;
+                            if(currLength > elem[0].positions[0].beginOffset){
+                                // The word is in this element.
+                                currLength -= pnode.innerText.length;
+                                let pos = elem[0].positions[0].beginOffset - currLength;
+                                let str = elem[0].positions[0].content;
+                                pnode.innerHTML = pnode.innerHTML.slice(0,pos) + '<span style="background-color: yellow;">' + pnode.innerHTML.slice(pos, pos+str.length) + '</span>' + pnode.innerHTML.slice(pos+str.length);
+                            }
+                        }
+                        pnode = node;
+                    }
                 }
             });
         }

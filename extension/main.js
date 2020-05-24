4e7e319337be7ad65b72c63ef111cc08338ce2fa
claby2 <wibow9770@gmail.com>
2020-05-24 14:59:29 +0800

    added popup style and integration

--- extension/main.js
+++ extension/main.js
@@ -3,8 +3,153 @@ function getAnalysis(_text) {
     .then(res => res.json())
 }
 
-
 let sleep = ms => new Promise((resolve, reject) => setTimeout(resolve, ms));
+
+
+let popupStyle = document.createElement('style');
+popupStyle.innerText = `
+@import url('https://rsms.me/inter/inter.css');
+html { font-family: 'Inter', sans-serif; }
+
+@supports (font-variation-settings: normal) {
+  html { font-family: 'Inter var', sans-serif; }
+}
+
+@font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://example.com/MaterialIcons-Regular.eot); /* For IE6-8 */
+    src: local('Material Icons'),
+        local('MaterialIcons-Regular'),
+        url(https://example.com/MaterialIcons-Regular.woff2) format('woff2'),
+        url(https://example.com/MaterialIcons-Regular.woff) format('woff'),
+        url(https://example.com/MaterialIcons-Regular.ttf) format('truetype');
+}
+
+.material-icons {
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;  /* Preferred icon size */
+    display: inline-block;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+
+    /* Support for all WebKit browsers. */
+    -webkit-font-smoothing: antialiased;
+    /* Support for Safari and Chrome. */
+    text-rendering: optimizeLegibility;
+
+    /* Support for Firefox. */
+    -moz-osx-font-smoothing: grayscale;
+
+    /* Support for IE. */
+    font-feature-settings: 'liga';
+}
+
+.viewfinder-popup {
+    visibility: hidden;
+    position: absolute;
+    font-family: 'Inter', sans-serif !important;
+    color: black;
+    box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    background-color: white;
+    padding: 20px 20px 20px 15px;
+    color: black !important;
+}
+
+.viewfinder-popup h2 {
+    color: black !important;
+    font-weight: 600 !important;
+    font-size: 1.5em !important;
+    margin: 0 0 5px 0;
+}
+
+.viewfinder-popup p {
+    color: black !important;
+    font-weight: 500 !important;
+    font-size: 1.3em !important;
+    opacity: 0.8 !important;
+}
+
+.viewfinder-popup div {
+    display: flex;
+    align-items: center;
+    text-align: left;
+    margin: 0 0 5px 0;
+}
+
+.viewfinder-popup div img {
+    margin: 0 20px 0 0;
+    opacity: 0.5;
+    width: 20px;
+}
+
+.viewfinder-word:hover {
+    cursor: pointer;
+}
+`;
+let moreIcon = `<img src = "https://cdn.discordapp.com/attachments/705022189359071263/714002361743441930/more_horiz-24px.svg" alt = "Read Icon">`;
+let chatIcon = `<img src = "https://cdn.discordapp.com/attachments/705022189359071263/714002359705141299/chat-24px.svg" "alt = "Chat Icon">`;
+
+document.body.appendChild(popupStyle);
+
+function appendPopup(elementProperties, analysis, randid) {
+    let popup = document.createElement('div');
+
+    popup.id = "viewfinder-popup-" + randid;
+
+    popup.classList.add("viewfinder-popup");
+    popup.style.left = (elementProperties.x + window.scrollX) + "px";
+    popup.style.top = (elementProperties.y + elementProperties.height + window.scrollY)+ "px";
+
+    let h2 = document.createElement('h2');
+    h2.innerText = "This topic seems controversial.";
+
+    let p = document.createElement('p');
+    p.style.margin = "0 0 20px 0";
+
+    let opinion;
+    let opinionColor;
+    switch(analysis.opinion) {
+        case "negative":
+            opinion = "oppose"
+            opinionColor = "#d32f2f";
+            break;
+        case "positive":
+            opinion = "support"
+            opinionColor = "#2E7D32";
+            break;
+        case "mixed":
+            opinion = "be mixed about"
+            opinionColor = "#f9a825";
+            break;
+        case "neutral":
+            opinion = "be neutral about"
+            opinionColor = "black";
+            break;
+    }
+    p.innerHTML = `This text seems to` + ` <span style = "color: ` + opinionColor + `; font-weight: 700;">` + opinion + `</span> ` + analysis.topic + ".";
+
+    let readMoreDiv = document.createElement('div');
+    readMoreDiv.innerHTML = moreIcon + `<p style = "opacity: 0.5 !important;"> Read more </p>`;
+
+    let chatDiv = document.createElement('div');
+    chatDiv.innerHTML = chatIcon + `<p style = "opacity: 0.5 !important;"> Discuss this topic </p>`;
+
+    popup.appendChild(h2);
+    popup.appendChild(p);
+    popup.appendChild(readMoreDiv);
+    popup.appendChild(chatDiv);
+
+    document.body.appendChild(popup);
+}
 
 setTimeout(function(){
     let text;
@@ -18,10 +163,13 @@ setTimeout(function(){
             posts[i] = arr[0]; // Set posts element as comment or post content
         }
 
+        var analysisObject;
+
         if(!(posts[i] === undefined)) {
             getAnalysis(posts[i].innerText).then(elem => {
                 if(elem.length !== 0) {
                     console.log(elem[0]);
+                    analysisObject = elem[0];
                     // Walk through the tree.
                     let nodes = document.createTreeWalker(posts[i], NodeFilter.SHOW_ALL, null, null);
                     // Current node, previous node
@@ -40,7 +188,7 @@ setTimeout(function(){
                                 let str = elem[0].positions[0].content;
                                 // Highlight the word.
                                 let rand = Math.random().toString();
-                                pnode.innerHTML = pnode.innerHTML.slice(0,pos) + '<span class = "viewfinder-word" id = "' + rand + '" style="background-color: yellow;">' + pnode.innerHTML.slice(pos, pos+str.length) + '</span>' + pnode.innerHTML.slice(pos+str.length);
+                                pnode.innerHTML = pnode.innerHTML.slice(0,pos) + '<span onmousedown = "if(document.getElementById(\'viewfinder-popup-' + rand + '\').style.visibility !== \'visible\') {document.getElementById(\'viewfinder-popup-' + rand + '\').style.visibility = \'visible\'} else {document.getElementById(\'viewfinder-popup-' + rand + '\').style.visibility = \'hidden\'}" class = "viewfinder-word" id = "' + rand + '" style="background-color: yellow;">' + pnode.innerHTML.slice(pos, pos+str.length) + '</span>' + pnode.innerHTML.slice(pos+str.length);
                                 return rand;
                             }
                         }
@@ -50,19 +198,9 @@ setTimeout(function(){
             }).then(randid => {
                 if(randid) {
                     let element = document.getElementById(randid);
-    
-                    let elementProperties = element.getBoundingClientRect();
-                    console.log(elementProperties);
-    
-                    let popup = document.createElement('div');
-    
-                    popup.style.position = 'absolute';
-                    popup.style.left = (elementProperties.x + window.scrollX) + "px";
-                    popup.style.top = (elementProperties.y + elementProperties.height + window.scrollY)+ "px";
-    
-                    popup.innerText = "Controversial Flag"; // temporary
-     
-                    document.body.appendChild(popup);
+                    console.log(analysisObject);
+
+                    appendPopup(element.getBoundingClientRect(), analysisObject, randid);
                 }
             });
         }
